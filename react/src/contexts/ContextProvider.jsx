454fc9453524814b2e68c96fbd38ef37b4ce4056
xHenrique22 <henrique.oliveira9823@gmail.com>
2023-02-01 19:47:08 -0300

    feat: notification and user form implemented

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -1,31 +1,48 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const StateContext = createContext({
     user: null,
     token: null,
+    notification: null,
     setUser: () => { },
-    setToken: () => { }
+    setToken: () => { },
+    setNotification: () => { }
 })
 
-
 export const ContextProvider = ({ children }) => {
-    const [user, setUser] = useState();
-    const [token, _setToken] = useState(null);
+    const [user, setUser] = useState({});
+    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+    const [notification, _setNotification] = useState('');
 
     const setToken = (token) => {
         _setToken(token)
         if (token) {
-            localStorage.setItem('ACESS_TOKEN', token);
+            localStorage.setItem('ACCESS_TOKEN', token);
         } else {
-            localStorage.removeItem('ACESS_TOKEN')
+            localStorage.removeItem('ACCESS_TOKEN');
         }
     }
 
+    const setNotification = message => {
+        _setNotification(message);
+
+        setTimeout(() => {
+            _setNotification('')
+        }, 5000)
+    }
 
     return (
-        <StateContext.Provider value={{ user, token, setUser, setToken }} >
+        <StateContext.Provider value={{
+            user,
+            setUser,
+            token,
+            setToken,
+            notification,
+            setNotification
+        }}>
             {children}
         </StateContext.Provider>
-    )
+    );
 }
-export const useStateContext = () => useContext(StateContext)
+
+export const useStateContext = () => useContext(StateContext);
---
+++ react/src/views/UserForm.jsx
@@ -0,0 +1,92 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axiosClient from "../axios-client.js";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+
+export default function UserForm() {
+    const navigate = useNavigate();
+    let { id } = useParams();
+    const [user, setUser] = useState({
+        id: null,
+        name: '',
+        email: '',
+        password: '',
+        password_confirmation: ''
+    })
+    const [errors, setErrors] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const { setNotification } = useStateContext()
+
+    if (id) {
+        useEffect(() => {
+            setLoading(true)
+            axiosClient.get(`/users/${id}`)
+                .then(({ data }) => {
+                    setLoading(false)
+                    setUser(data)
+                })
+                .catch(() => {
+                    setLoading(false)
+                })
+        }, [])
+    }
+
+    const onSubmit = ev => {
+        ev.preventDefault()
+        if (user.id) {
+            axiosClient.put(`/users/${user.id}`, user)
+                .then(() => {
+                    setNotification('O usuário foi atualizado com sucesso')
+                    navigate('/users')
+                })
+                .catch(err => {
+                    const response = err.response;
+                    if (response && response.status === 422) {
+                        setErrors(response.data.errors)
+                    }
+                })
+        } else {
+            axiosClient.post('/users', user)
+                .then(() => {
+                    setNotification('O usuário foi criado com sucesso')
+                    navigate('/users')
+                })
+                .catch(err => {
+                    const response = err.response;
+                    if (response && response.status === 422) {
+                        setErrors(response.data.errors)
+                    }
+                })
+        }
+    }
+
+    return (
+        <>
+            {user.id && <h1>Atualizar usuário: {user.name}</h1>}
+            {!user.id && <h1>Novo usuário</h1>}
+            <div className="card animated fadeInDown">
+                {loading && (
+                    <div className="text-center">
+                        Carregando...
+                    </div>
+                )}
+                {errors &&
+                    <div className="alert">
+                        {Object.keys(errors).map(key => (
+                            <p key={key}>{errors[key][0]}</p>
+                        ))}
+                    </div>
+                }
+                {!loading && (
+                    <form onSubmit={onSubmit}>
+                        <input value={user.name} onChange={ev => setUser({ ...user, name: ev.target.value })} placeholder="Nome" />
+                        <input value={user.email} onChange={ev => setUser({ ...user, email: ev.target.value })} placeholder="Email" />
+                        <input type="password" onChange={ev => setUser({ ...user, password: ev.target.value })} placeholder="Senha" />
+                        <input type="password" onChange={ev => setUser({ ...user, password_confirmation: ev.target.value })} placeholder="Confirmar senha" />
+                        <button className="btn">Salvar</button>
+                    </form>
+                )}
+            </div>
+        </>
+    )
+}
